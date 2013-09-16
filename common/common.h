25ebe528f8d318d6f1b36c1df7fe7e148808a96c
KIHARA Hideto <deton@m1.interq.or.jp>
2013-09-16 10:25:42 +0900

    名前をtsf-tutcodeからtsf-vimに変更。クラスIDも変更。

--- common/common.h
+++ common/common.h
@@ -2,8 +2,8 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-#define TEXTSERVICE_NAME	L"tsf-tutcode"
-#define TEXTSERVICE_VER		L"0.0.4"
+#define TEXTSERVICE_NAME	L"tsf-vim"
+#define TEXTSERVICE_VER		L"0.0.1"
 
 #ifndef _DEBUG
 #define TEXTSERVICE_DESC	TEXTSERVICE_NAME
@@ -13,9 +13,9 @@
 
 //for resource
 #define RC_AUTHOR			"KIHARA Hideto"
-#define RC_PRODUCT			"tsf-tutcode"
-#define RC_VERSION			"0.0.4"
-#define RC_VERSION_D		0,0,4,0
+#define RC_PRODUCT			"tsf-vim"
+#define RC_VERSION			"0.0.1"
+#define RC_VERSION_D		0,0,1,0
 
 #define DICBUFSIZE			0x2000
 #define PIPEBUFSIZE			0x2000
@@ -45,12 +45,12 @@
 #define REP_OK			L'1'	//hit
 #define REP_FALSE		L'4'	//nothing
 
-#define CORVUSMGREXE		L"imtutmgr.exe"
-#define CORVUSCNFEXE		L"imtutcnf.exe"
+#define CORVUSMGREXE		L"imvimmgr.exe"
+#define CORVUSCNFEXE		L"imvimcnf.exe"
 #ifndef _DEBUG
-#define CORVUSKRNLOBJ		L"tsf-tutcode-"
+#define CORVUSKRNLOBJ		L"tsf-vim-"
 #else
-#define CORVUSKRNLOBJ		L"tsf-tutcode-debug-"
+#define CORVUSKRNLOBJ		L"tsf-vim-debug-"
 #endif
 #define CORVUSMGRMUTEX		CORVUSKRNLOBJ L"mgr-"
 #define CORVUSCNFMUTEX		CORVUSKRNLOBJ L"cnf-"
