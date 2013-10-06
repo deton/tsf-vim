e4abd147a606f063b13d8c9186f8bbce4a16e36f
KIHARA Hideto <deton@m1.interq.or.jp>
2013-10-06 11:57:15 +0900

    追加もれのViKeyHandler.hを追加。

---
+++ imcrvtip/ViKeyHandler.h
@@ -0,0 +1,39 @@
+#ifndef VIKEYHANDLER_H
+#define VIKEYHANDLER_H
+
+#include "mozc/win32/base/deleter.h"
+#include "ViCmd.h"
+
+class CTextService;
+
+class ViKeyHandler
+{
+public:
+	ViKeyHandler(CTextService *textService);
+	~ViKeyHandler();
+
+	void Reset();
+	BOOL IsWaitingNextKey();
+	HRESULT HandleKey(TfEditCookie ec, ITfContext *pContext, WCHAR ch);
+
+private:
+	void _HandleFunc(TfEditCookie ec, ITfContext *pContext, WCHAR ch);
+	void _QueueKey(std::vector<INPUT> *inputs, UINT vk, int count = 1);
+	void _QueueKeyForSelection(std::vector<INPUT> *inputs);
+	void _QueueKeyForModifier(std::vector<INPUT> *inputs, UINT vk, BOOL up);
+	void _QueueKeyWithControl(std::vector<INPUT> *inputs, UINT vk);
+	void _SendKey(UINT vk, int count = 1);
+	void _SendKeyWithControl(UINT vk);
+	void _ViOpOrMove(UINT vk, int count);
+	void _Vi_o();
+	void _Vi_p(ITfContext *pContext);
+	void _Vi_P();
+	void _ViNextSentence(ITfContext *pContext);
+	void _Vi_f(ITfContext *pContext, WCHAR ch);
+
+	CTextService *_textService;
+	ViCmd vicmd;
+	mozc::win32::VKBackBasedDeleter deleter;
+	std::unique_ptr<mozc::win32::Win32KeyboardInterface> keyboard_;
+};
+#endif //VIKEYHANDLER_H
