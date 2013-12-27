e3f1ad67e9c8c29fd9a1d3dcb57885cba84c8667
KIHARA Hideto <deton@m1.interq.or.jp>
2013-12-27 17:48:26 +0900

    Update README: update release date. Change _mkarc.cmd to make zip archive and add README_JA.TXT.

--- installer/_mkarc.cmd
+++ installer/_mkarc.cmd
@@ -2,6 +2,6 @@
 
 pushd %~dp0
 
-7z.exe a tsfvim.7z tsfvim-x64.msi tsfvim-x86.msi ..\README.TXT ..\LICENSE.TXT
+7z.exe a -tzip tsf-vim.zip tsfvim-x64.msi tsfvim-x86.msi ..\README.TXT ..\README_JA.TXT ..\LICENSE.TXT
 
 popd
